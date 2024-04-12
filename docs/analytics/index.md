79ebed369eb369d36ff19856f06816e6592f5a84
Michael Herger <mherger@logitech.com>
2024-04-12 17:00:12 +0200

    Add some historical data to the stats charts

--- docs/analytics/index.md
+++ docs/analytics/index.md
@@ -3,14 +3,45 @@ layout: default
 title: Some Numbers about LMS Installations
 ---
 
+!!! note
+    Please note that analytics reporting currently is opt-in. The following numbers
+    therefore can't be considered representative. Only the adventurous running the
+    latest and greatest nightly builds who went to Settings/Plugins to enable the
+    "Report Analytics Data" plugin are represented here.
+
 [Learn more about how this data is gathered](learn-more.md)
 
-## Countries
+## Number of LMS Installations
+
+(with stats enabled anyway...)
 
 ```mermaid
-%%{ {{ pieStyles }} }%%
-pie title Countries
-{{ countries }}
+---
+config:
+    xyChart:
+        height: 200
+    {{ xyBarStyles }}
+---
+xychart-beta
+    x-axis [{{ histDates }}]
+    y-axis 0 --> 200
+    line [{{ histInstallations }}]
+```
+
+
+## Connected Players
+
+```mermaid
+---
+config:
+    xyChart:
+        height: 200
+    {{ xyBarStyles }}
+---
+xychart-beta
+    x-axis [{{ histDates }}]
+    y-axis 0 --> 500
+    line [{{ histPlayers }}]
 ```
 
 
@@ -20,6 +51,15 @@ pie title Countries
 %%{ {{ pieStyles }} }%%
 pie title LMS Versions
 {{ versions }}
+```
+
+
+## Countries
+
+```mermaid
+%%{ {{ pieStyles }} }%%
+pie title Countries
+{{ countries }}
 ```
 
 
--- tools/get-analytics-data.pl
+++ tools/get-analytics-data.pl
@@ -8,22 +8,34 @@ use LWP::UserAgent;
 use YAML;
 
 use constant STATS_SUMMARY => 'https://stats.lms-community.org/api/stats';
-use constant STATS_YAML => 'docs/analytics/stats.yaml';
+use constant STATS_HISTORY => 'https://stats.lms-community.org/api/stats/history';
+use constant STATS_YAML    => 'docs/analytics/stats.yaml';
 
-my $stats;
+my ($stats, $history);
+
 eval {
     my $ua = LWP::UserAgent->new();
-    $ua->ssl_opts(verify_hostname => 0);
+    # $ua->ssl_opts(verify_hostname => 0);
     my $resp = $ua->get(STATS_SUMMARY);
-	$stats = from_json($resp->content);
+	$stats   = from_json($resp->content);
+
+    $resp    = $ua->get(STATS_HISTORY);
+	$history = from_json($resp->content);
 } || die "$@";
 
-my %stats;
-$stats{versions}     = join("\n", map { my ($k, $v) = each %$_; qq("$k": $v) } @{$stats->{versions} || []});
-$stats{countries}    = join("\n", map { my ($k, $v) = each %$_; qq("$k": $v) } @{$stats->{countries} || []});
-$stats{os}           = join("\n", map { my ($k, $v) = each %$_; qq("$k": $v) } @{$stats->{os} || []});
+my $c;
+my %stats = (
+    versions  => join("\n", map { my ($k, $v) = each %$_; qq("$k": $v) } @{$stats->{versions} || []}),
+    countries => join("\n", map { my ($k, $v) = each %$_; qq("$k": $v) } @{$stats->{countries} || []}),
+    os        => join("\n", map { my ($k, $v) = each %$_; qq("$k": $v) } @{$stats->{os} || []}),
 
-$stats{pluginLabels} = join(',', map { sprintf('"%s"', keys %$_) } @{$stats->{plugins} || []});
-$stats{pluginCounts} = join(',', map { (values %$_)[0] } @{$stats->{plugins} || []});
+    pluginLabels => join(',', map { sprintf('"%s"', keys %$_) } @{$stats->{plugins} || []}),
+    pluginCounts => join(',', map { (values %$_)[0] } @{$stats->{plugins} || []}),
+
+    histDates   => join(',', map { sprintf('"%s"', $_->{d}) } @$history),
+    # get the total number of installations from the versions counts
+    histInstallations => join(',', map { $c = 0; map { my ($k, $v) = each %$_; $c+=$v } @{from_json($_->{v})}; $c } @$history),
+    histPlayers => join(',', map { $_->{p} } @$history),
+);
 
 YAML::DumpFile(STATS_YAML, \%stats);
