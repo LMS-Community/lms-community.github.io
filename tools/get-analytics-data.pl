6f51fad915f39e16403e6dfa3deee587bd977d2e
Michael Herger <michael@herger.net>
2024-04-16 09:37:26 +0200

    Add counts of connected players

--- tools/get-analytics-data.pl
+++ tools/get-analytics-data.pl
@@ -88,6 +88,7 @@ my %stats = (
     versions  => \@versions,
     players   => \@players,
     playerTypes => prepareData($stats->{playerTypes}, 0, 'p'),
+    connectedPlayers => prepareData($stats->{connectedPlayers}, 0, 'p'),
     countries => prepareData($stats->{countries}, 0, 'c', 'i'),
     os        => prepareData($stats->{os}, 6, 'o'),
     plugins   => prepareData($stats->{plugins}, 10, 'p'),
