174074f432907d55ca93e03216b30a212419e810
Michael Herger <michael@herger.net>
2025-10-25 12:36:49 +0200

    Stop messing with manual parsing of paths in the downloads handler, when `hono` can do all of this and more for us. Align with the other workers and use that framework.

--- tools/downloads-handler/src/index.ts
+++ tools/downloads-handler/src/index.ts
@@ -1,10 +1,12 @@
-// see https://developers.cloudflare.com/r2/examples/demo-worker/
+import { Hono, Context } from 'hono'
+
+const urlPrefix = 'downloads/listing/archive';
 
 interface Env {
     DOWNLOADS_BUCKET: R2Bucket
 }
 
-const allowedPrefixes = [
+const allowedPrefixes: Array<string> = [
     'LyrionMusicServer_v.*',
     'LogitechMediaServer_v.*',
     'SqueezeboxServer_v.*',
@@ -15,52 +17,51 @@ const allowedPrefixes = [
     'docs/*',
 ]
 
-const corsHeaders = {
+const corsHeaders: Record<string, string> = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, OPTIONS, HEAD",
     "Access-Control-Max-Age": "86400",
 }
 
-const urlPrefix = 'downloads/listing/archive/';
+const app = new Hono()
 
-export default {
-    async fetch(request, env): Promise<Response> {
-        const url = new URL(request.url)
-        const prefix = url.pathname.slice(1).replace(urlPrefix, '')
+app.on('GET', [`/${urlPrefix}/:prefix`, '/:prefix'], async (c: Context, ) => {
+    const prefix = c.req.param('prefix');
 
-        if (!prefix) {
-            return Response.redirect('https://lyrion.org/getting-started')
-        }
-
-        if (request.method === 'GET' && prefix && allowedPrefixes.some(p => new RegExp(p).test(prefix))) {
-            const listing = await env.DOWNLOADS_BUCKET.list({ prefix })
-
-            // we only want object with the given prefix, but no sub-folders
-            const filterRe = new RegExp(prefix + (prefix.match(new RegExp("/$")) ? "" : "/") + "[^\/]+$")
-
-            const body = listing.objects
-                .filter(i => filterRe.test(i.key))
-                .map(i => ({
-                    key: i.key,
-                    size: i.size,
-                }))
-
-            return new Response(JSON.stringify(body), {
-                headers: {
-                    ...corsHeaders,
-                    'Access-Control-Allow-Headers': request.headers.get(
-                      "Access-Control-Request-Headers",
-                    ) || '',
-                    'content-type': 'application/json; charset=UTF-8',
-                }
-            })
-        }
-
-        // we don't want to reveal a path doesn't exist - return valid, but empty result set
-        return new Response('[]', {
-            headers: {
-                'content-type': 'application/json; charset=UTF-8',
-            }
-        })
+    if (!prefix) {
+        return Response.redirect('https://lyrion.org/getting-started')
     }
-} satisfies ExportedHandler<Env>
+
+    if (prefix && allowedPrefixes.some(p => new RegExp(p).test(prefix))) {
+        const listing = await c.env.DOWNLOADS_BUCKET.list({ prefix })
+
+        // we only want objects with the given prefix, but no sub-folders
+        const filterRe = new RegExp(prefix + (prefix.match(new RegExp("/$")) ? "" : "/") + "[^\/]+$")
+
+        const body = listing.objects
+            .filter((i: any) => filterRe.test(i.key))
+            .map((i: any) => ({
+                key: i.key,
+                size: i.size,
+            }))
+
+        Object.keys(corsHeaders).forEach(key => {
+            c.header(key, corsHeaders[key]);
+        })
+
+        c.header('Access-Control-Allow-Headers', c.req.header(
+            "Access-Control-Request-Headers",
+        ) || '')
+
+        return c.json(body)
+    }
+
+    // we don't want to reveal a path doesn't exist - return valid, but empty result set
+    return new Response('[]', {
+        headers: {
+            'content-type': 'application/json; charset=UTF-8',
+        }
+    })
+})
+
+export default app
